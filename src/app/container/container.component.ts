a0377931d7d58c7e2eaa8f8f4588e221b80151aa
loga-krishna <logakrishnad@gmail.com>
2024-07-11 17:27:33 +0530

    Main Commit

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SearchComponent } from "./search/search.component";
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from "./product-list/product-list.component";
+
+
+@Component({
+  selector: 'app-container',
+  standalone: true,
+  imports: [SearchComponent, CommonModule, ProductListComponent],
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+   name:string="loga";
+}
---
+++ src/app/container/product-list/filter/filter.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css'
+})
+export class FilterComponent {
+  @Input()
+  all:number=0
+
+  @Input()
+  instock:number=0;
+
+  @Input()
+  outstock:number=0;
+  
+  selectedbutton:string='all';
+
+  @Output()
+  selectedEvent:EventEmitter<string>=new EventEmitter<string>();
+  
+  eventemmitted(){
+    console.log(this.selectedbutton+" from child");
+    
+    this.selectedEvent.emit(this.selectedbutton);
+    //console.log(this.selectedbutton);
+  }
+}
